9870eec814318940009c691cd50fb88e0c657efc
Hera Bonita <116147053+gwynnbonita@users.noreply.github.com>
2023-06-16 08:33:35 +0800

    Create sleep_in.py

---
+++ Warmup-1/sleep_in.py
@@ -0,0 +1,21 @@
+# Warmup-1 > sleep_in
+
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True if we
+# are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we
+# sleep in.
+
+def sleep_in(weekday, vacation):
+  if (not weekday) or (vacation == True):
+    return True
+  else:
+    return False
+ 
+"""
+Solution:
+def sleep_in(weekday, vacation):
+  if not weekday or vacation:
+    return True
+  else:
+    return False
+  # This can be shortened to: return(not weekday or vacation)
+"""
